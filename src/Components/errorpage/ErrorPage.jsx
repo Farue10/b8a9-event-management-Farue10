e77f389a82025e254a72e2b68f134071348bd09a
Faruqe10 <138726400+Farue10@users.noreply.github.com>
2023-10-08 22:14:06 +0600

    Add files via upload
    error page added project and show this

--- src/Components/errorpage/ErrorPage.jsx
+++ src/Components/errorpage/ErrorPage.jsx
@@ -1,9 +1,10 @@
 
-
+import backgorundImg1 from '../../assets/error.png'
 const ErrorPage = () => {
     return (
-        <div>
+        <div className='max-w-3xl mx-auto'>
            <h2 className="text-center text-5xl text-red-700 font-medium">this page is 404!!!</h2>
+           <img src={backgorundImg1}></img>
         </div>
     );
 };
